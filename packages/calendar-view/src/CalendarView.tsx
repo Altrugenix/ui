72914218aec5ff46aeae2ad1a9e4c8fafe3b6c6b
Alchie Tagudin <chesteralantagudin@gmail.com>
2026-04-30 11:52:12 +0800

    refactor: remove unused React import statements from CalendarView components

--- packages/calendar-view/src/CalendarView.tsx
+++ packages/calendar-view/src/CalendarView.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { cn } from "@altrugenix/core";
 import { ChevronLeft, ChevronRight, Plus } from "lucide-react";
 import { Button } from "@altrugenix/button";
